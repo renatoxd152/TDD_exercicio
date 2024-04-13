6967f8587099929e7627a7bc2d2a8fbbffc71032
renatoxd152 <renatomorillo@gmail.com>
2024-04-12 21:28:58 -0300

    refactor: Implementado uma condição para os vetores se forem nulos ou vazios

--- src/main/java/SomarVetor.java
+++ src/main/java/SomarVetor.java
@@ -2,10 +2,9 @@ public class SomarVetor {
 
     public int[] somar(int[] a, int[]b)
     {
-        if(a == null && b == null) return new int[]{};
-
+        if(a == null || a.length == 0 || b == null || b.length == 0)
+            return new int[]{};
         return new int[]{};
-
     }
 
 }
