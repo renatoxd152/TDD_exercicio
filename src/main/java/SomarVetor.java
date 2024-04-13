ab1c32ade6205d9038816c52d9cc558ba8e26f1b
renatoxd152 <renatomorillo@gmail.com>
2024-04-13 02:06:35 -0300

    feat: Tratamento realizado para retornar a soma dos vetores

--- src/main/java/SomarVetor.java
+++ src/main/java/SomarVetor.java
@@ -6,7 +6,15 @@ public class SomarVetor {
             return new int[]{};
         if(a.length != 5 || b.length != 5)
             throw new IllegalArgumentException("A and B lengths are not equal");
-        return new int[]{};
+
+        int[] c = new int[5];
+        for (int i = 0; i < 5; i++) {
+            c[i] = a[i] + b[i];
+        }
+        return c;
+
+
+
     }
 
 }
