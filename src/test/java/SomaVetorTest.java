42e37bfceb520131e6b7663343d5a0a3fb6316a1
renatoxd152 <renatomorillo@gmail.com>
2024-04-12 22:15:32 -0300

    test: Se os vetores forem de tamanhos diferentes, deve-se retornar um array vazio

--- src/test/java/SomaVetorTest.java
+++ src/test/java/SomaVetorTest.java
@@ -34,4 +34,18 @@ public class SomaVetorTest {
         assertArrayEquals(c, somarVetor.somar(a,b));
     }
 
+    @Test
+    @DisplayName("Deve retornar um vetor vazio para vetores de tamanhos diferentes")
+
+    public void vetoresDiferentes()
+    {
+        SomarVetor somarVetor = new SomarVetor();
+        int[] a = {2,5,10,190};
+        int[] b = {2,4};
+
+        int[] c = {};
+
+        assertArrayEquals(c, somarVetor.somar(a, b));
+    }
+
 }
