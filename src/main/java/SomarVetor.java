bbfd8852dd9ace376b928c2e22766533a421f9c3
renatoxd152 <renatomorillo@gmail.com>
2024-04-13 15:14:10 -0300

    feat: Exceção adicionada se algum dos vetores forem nulos ou vazios

--- src/main/java/SomarVetor.java
+++ src/main/java/SomarVetor.java
@@ -3,9 +3,9 @@ public class SomarVetor {
     public int[] somar(int[] a, int[]b)
     {
         if(a == null || a.length == 0 || b == null || b.length == 0)
-            return new int[]{};
+            throw new IllegalArgumentException("O vetor não pode ser nulo ou vazio!");
         if(a.length != 5 || b.length != 5)
-            throw new IllegalArgumentException("A and B lengths are not equal");
+            throw new IllegalArgumentException("A ou B não possuem o tamanho 5!");
 
         int[] c = new int[5];
         for (int i = 0; i < 5; i++) {
